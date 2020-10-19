3ac4e52d86b0508c9a31613f371955e401c18846
Simon Niklaus <simon.niklaus@outlook.com>
2020-10-19 09:56:35 -0700

    no message

--- benchmark.py
+++ benchmark.py
@@ -27,35 +27,35 @@ if os.path.exists('./netflix') == False:
 # end
 
 if len(glob.glob('./netflix/BoxingPractice-*.png')) != 100:
-    os.system('ffmpeg -i https://media.xiph.org/video/derf/ElFuente/Netflix_BoxingPractice_4096x2160_60fps_10bit_420.y4m -pix_fmt rgb8 -vframes 100 ./netflix/BoxingPractice-%03d.png')
+    os.system('ffmpeg -i https://media.xiph.org/video/derf/ElFuente/Netflix_BoxingPractice_4096x2160_60fps_10bit_420.y4m -pix_fmt rgb24 -vframes 100 ./netflix/BoxingPractice-%03d.png')
 # end
 
 if len(glob.glob('./netflix/Crosswalk-*.png')) != 100:
-    os.system('ffmpeg -i https://media.xiph.org/video/derf/ElFuente/Netflix_Crosswalk_4096x2160_60fps_10bit_420.y4m -pix_fmt rgb8 -vframes 100 ./netflix/Crosswalk-%03d.png')
+    os.system('ffmpeg -i https://media.xiph.org/video/derf/ElFuente/Netflix_Crosswalk_4096x2160_60fps_10bit_420.y4m -pix_fmt rgb24 -vframes 100 ./netflix/Crosswalk-%03d.png')
 # end
 
 if len(glob.glob('./netflix/DrivingPOV-*.png')) != 100:
-    os.system('ffmpeg -i https://media.xiph.org/video/derf/Chimera/Netflix_DrivingPOV_4096x2160_60fps_10bit_420.y4m -pix_fmt rgb8 -vframes 100 ./netflix/DrivingPOV-%03d.png')
+    os.system('ffmpeg -i https://media.xiph.org/video/derf/Chimera/Netflix_DrivingPOV_4096x2160_60fps_10bit_420.y4m -pix_fmt rgb24 -vframes 100 ./netflix/DrivingPOV-%03d.png')
 # end
 
 if len(glob.glob('./netflix/FoodMarket-*.png')) != 100:
-    os.system('ffmpeg -i https://media.xiph.org/video/derf/ElFuente/Netflix_FoodMarket_4096x2160_60fps_10bit_420.y4m -pix_fmt rgb8 -vframes 100 ./netflix/FoodMarket-%03d.png')
+    os.system('ffmpeg -i https://media.xiph.org/video/derf/ElFuente/Netflix_FoodMarket_4096x2160_60fps_10bit_420.y4m -pix_fmt rgb24 -vframes 100 ./netflix/FoodMarket-%03d.png')
 # end
 
 if len(glob.glob('./netflix/FoodMarket2-*.png')) != 100:
-    os.system('ffmpeg -i https://media.xiph.org/video/derf/ElFuente/Netflix_FoodMarket2_4096x2160_60fps_10bit_420.y4m -pix_fmt rgb8 -vframes 100 ./netflix/FoodMarket2-%03d.png')
+    os.system('ffmpeg -i https://media.xiph.org/video/derf/ElFuente/Netflix_FoodMarket2_4096x2160_60fps_10bit_420.y4m -pix_fmt rgb24 -vframes 100 ./netflix/FoodMarket2-%03d.png')
 # end
 
 if len(glob.glob('./netflix/RitualDance-*.png')) != 100:
-    os.system('ffmpeg -i https://media.xiph.org/video/derf/ElFuente/Netflix_RitualDance_4096x2160_60fps_10bit_420.y4m -pix_fmt rgb8 -vframes 100 ./netflix/RitualDance-%03d.png')
+    os.system('ffmpeg -i https://media.xiph.org/video/derf/ElFuente/Netflix_RitualDance_4096x2160_60fps_10bit_420.y4m -pix_fmt rgb24 -vframes 100 ./netflix/RitualDance-%03d.png')
 # end
 
 if len(glob.glob('./netflix/SquareAndTimelapse-*.png')) != 100:
-    os.system('ffmpeg -i https://media.xiph.org/video/derf/ElFuente/Netflix_SquareAndTimelapse_4096x2160_60fps_10bit_420.y4m -pix_fmt rgb8 -vframes 100 ./netflix/SquareAndTimelapse-%03d.png')
+    os.system('ffmpeg -i https://media.xiph.org/video/derf/ElFuente/Netflix_SquareAndTimelapse_4096x2160_60fps_10bit_420.y4m -pix_fmt rgb24 -vframes 100 ./netflix/SquareAndTimelapse-%03d.png')
 # end
 
 if len(glob.glob('./netflix/Tango-*.png')) != 100:
-    os.system('ffmpeg -i https://media.xiph.org/video/derf/ElFuente/Netflix_Tango_4096x2160_60fps_10bit_420.y4m -pix_fmt rgb8 -vframes 100 ./netflix/Tango-%03d.png')
+    os.system('ffmpeg -i https://media.xiph.org/video/derf/ElFuente/Netflix_Tango_4096x2160_60fps_10bit_420.y4m -pix_fmt rgb24 -vframes 100 ./netflix/Tango-%03d.png')
 # end
 
 ##########################################################
